5d1d11c4a54cff2d8ae75fa89f1d31a4054bc359
Keviinplz <kevin.pinochet@ug.uchile.cl>
2022-10-23 18:48:50 -0300

    [FIX] Adding /api/trpc to vercel url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -39,7 +39,7 @@ const MyApp = ({
 export default withTRPC<AppRouter>({
   config({ ctx }) {
     const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
       : "http://localhost:3000/api/trpc";
 
     return {
